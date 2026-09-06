d727f2b5b4109194e00e94fd377845751f4a186e
bluesailor <bluesailor@users.noreply.github.com>
2026-09-06 12:36:17 +0800

    build: record merged main provenance

---
+++ config/build.php
@@ -0,0 +1,5 @@
+<?php
+
+declare(strict_types=1);
+
+return '1.19.7-20260906123556';
---
+++ config/provenance.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+return array (
+  'schema' => 1,
+  'vendor_id' => 'cn.yikai',
+  'product_id' => 'cn.yikai.yikaicms',
+  'product_name' => 'YikaiCMS',
+  'copyright_holder' => 'Yikai',
+  'license_id' => 'YIKAI-CMS-LICENSE-1.0',
+  'version' => '1.19.7',
+  'build_id' => '1.19.7-20260906123556',
+  'built_at' => '2026-09-06T04:35:56+00:00',
+  'source_commit' => '41b12ee7',
+  'source_dirty' => false,
+  'core_tree_sha256' => '7afda72e9cc1bd73000ec453581c789a20b6f3680be8a691a607c0b5be75a300',
+  'core_files' => 
+  array (
+    'config/product.php' => 'bdf457bf45f9433271d863b103a66fa3e19873fd05dd799b5d413c4def10dc1a',
+    'config/version.php' => 'c15a0299b057f7d5481c2a67978b65e76dc3981dae25ae3a4ab72a88fc454416',
+    'includes/FooterNavigation.php' => 'd4fc42148b7d78f9c424fd08da90b2b2e98fc7a42edf233d02955f52230b8393',
+    'includes/License.php' => 'ddee564d4f72ef76a69f3f5a43504b3777b7905a30b6be4f2219a1ec65627bba',
+    'includes/ProductIdentity.php' => '8257c0c5a8c0b71a28ba9884f5dfb8574d0e43e1ed16beec20d15b32f9fadd19',
+    'includes/builder/BloxDocumentPipeline.php' => '166f081225dd922f13d345c65afc3887c338a53ff2bbd574803f558a35f59b31',
+    'includes/builder/BloxTemplateImporter.php' => 'fef4e6e2d4548fe0d89fff57034235e4ad32641d6d677e2b3a3e54d5e927a794',
+    'includes/functions.php' => '8887744f57b5fc74e0468145186410fa069048b91c50741dd73aa2ae373d84ec',
+    'includes/init.php' => 'a24d28ca8a8c7bb5cc22b091c072aaa38a34afa385a010ed8a63bca316cfd8fd',
+    'includes/security.php' => '4da290e3a64ddd6bc31e45fb0e7ebebb4fa268e2f2eccf90dc035322f5fb5213',
+  ),
+);
